a99fb2972f60e56b749d076a30fe0327f256b60f
Jacqueline Law <jacqueline.n.law@gmail.com>
2016-04-04 00:40:29 +0000

    added onclick handler to trigger refresh function

--- random/js/random.js
+++ random/js/random.js
@@ -1,14 +1,16 @@
-function randomize() {
+var randomize = function () {
   return Math.floor(Math.random() * 255);
 };
 
-var r = randomize();
-var g = randomize();
-var b = randomize();
+var refresh = function () {
+  var r = randomize();
+  var g = randomize();
+  var b = randomize();
 
-var color = 'rgb(' + r +',' + g + ',' + b + ')';
+  var color = 'rgb(' + r +',' + g + ',' + b + ')';
+  document.body.style.backgroundColor = color;
+  document.getElementById('color-value').textContent = color;
+  document.body.style.color = color;
+}
 
-document.body.style.backgroundColor = color;
-document.getElementById('color-value').textContent = color;
-document.body.style.color = color;
-
+refresh();
